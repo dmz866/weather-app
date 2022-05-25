b9f46973963db2ccf5febdc5c281e9575180c02e
David Murillo <dmz8667@gmail.com>
2022-05-25 18:55:38 -0500

    user service created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteConstants } from './core/constants/route-constants';
+import { AuthGuard } from './core/guards/auth/auth.guard';
 
 const routes: Routes = [
 	{ path: RouteConstants.LOGIN_ROUTE, loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule) },
-	{ path: RouteConstants.WEATHER_ROUTE, loadChildren: () => import('./pages/weather-page/weather-page.module').then(m => m.WeatherPageModule) },
+	{ path: RouteConstants.WEATHER_ROUTE, loadChildren: () => import('./pages/weather-page/weather-page.module').then(m => m.WeatherPageModule), canActivate: [AuthGuard] },
 	{ path: '**', redirectTo: RouteConstants.LOGIN_ROUTE },
 ];
 
